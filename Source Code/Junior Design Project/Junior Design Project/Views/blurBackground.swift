f63b3f127d0e8215686fd375a53c3be9bae090c4
Asha Das <asha.das@northsouth.edu>
2019-07-02 12:34:14 +0600

    Segueway Added
    Size of the background of the text Field need to be fixed.

--- Source Code/Junior Design Project/Junior Design Project/Views/blurBackground.swift
+++ Source Code/Junior Design Project/Junior Design Project/Views/blurBackground.swift
@@ -14,7 +14,7 @@ class CurrencyTxtField: UITextField {
     
     override func draw(_ rect: CGRect) {
         let size: CGFloat = 20
-        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: 30))
         currencyLbl.backgroundColor = #colorLiteral(red: 0.8840534687, green: 0.8784657121, blue: 0.8883432746, alpha: 0.7970675616)
         currencyLbl.textAlignment = .center
         currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
